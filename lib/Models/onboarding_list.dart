e230dab6ab97e37cd2a0c2c12b8f0949d3d4c3e2
OmarShatla11 <125584814+OmarShatla11@users.noreply.github.com>
2025-11-01 20:34:45 +0200

    Added Onboarding list

---
+++ lib/Models/onboarding_list.dart
@@ -0,0 +1,22 @@
+import '../models/onboarding_data.dart';
+
+final List<OnboardingData> onboardingList = [
+  const OnboardingData(
+    image: "assets/flower3.jpg",
+    title1: "Welcome to",
+    title2: "FLORELA",
+    desc: "Discover the beauty of flowers and let nature inspire your moments.",
+  ),
+  const OnboardingData(
+    image: "assets/flower5.jpg",
+    title1: "Fresh Blooms,",
+    title2: "Endless Joy",
+    desc: "Explore unique floral designs crafted with love and elegance.",
+  ),
+  const OnboardingData(
+    image: "assets/flower4.jpg",
+    title1: "Start Your",
+    title2: "FLORELA Journey",
+    desc: "Bring colors to life — shop, share, and celebrate every occasion.",
+  ),
+];
